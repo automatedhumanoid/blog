f560f8efea55389a81658763ff33107df6dc7b02
Samuel Aslam <samuelaslam@Sams-MacBook-Pro-2.local>
2020-04-06 13:34:24 -0400

    blog

---
+++ mysite/mysite/blog/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200,unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User,on_delete=models.CASCADE,related_name='blog_posts')
+    content = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+
+        ordering = ['-created']
+
+
+    def __str__(self):
+
+        return self.title
---
+++ mysite/mysite/blog/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.views import generic
+from .models import Post
+
+class PostList(generic.ListView):
+
+    queryset = Post.objects.order_by('-created')
+    template_name = 'index.html'
+
+
+class PostDetail(generic.DetailView):
+    
+    model = Post
+    template_name = 'post_detail.html'
+
